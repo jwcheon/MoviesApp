0796abbb633d5c26915cfe6f0f542af8139f0788
Jinwoo Cheon <jinwoocheon@yonsei.ac.kr>
2022-09-25 21:17:38 +0900

    Using hooks to load Asset & Image (simpler way, but restricts 'startAsync' part)

--- App.js
+++ App.js
@@ -1,37 +1,17 @@
 import React, { useState } from 'react';
 import AppLoading from 'expo-app-loading'; // actually depreacted
 import * as SplashScreen from 'expo-splash-screen'; // should change to this later
-import * as Font from 'expo-font';
+import { useFonts } from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
-import { Asset } from 'expo-asset';
+import { Asset, useAssets } from 'expo-asset';
 import { Text, Image } from 'react-native';
 
-const loadFonts = (fonts) => fonts.map(font => Font.loadAsync(font));
-const loadImages = (images) => images.map(image => {
-  if (typeof image === "string") {
-    return Image.prefetch(image);
-  } else {
-    return Asset.loadAsync(image);
-  }
-})
-
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true); // called when AppLoading is finished
-  const startLoading = async() => {
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadImages([require('./image.png'), 'https://d33wubrfki0l68.cloudfront.net/554c3b0e09cf167f0281fda839a5433f2040b349/ecfc9/img/header_logo.svg'])
-    await Promise.all([...fonts, ...images]);
-  };
+  const [assets] = useAssets([require('./image.png')]);
+  const [loaded] = useFonts(Ionicons.font); // this cannot do prefetch (fetching img from servers)
 
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
 
   return (
