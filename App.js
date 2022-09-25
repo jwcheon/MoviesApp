ccb044decadc42a895ac3a600c3a772fa4665572
Jinwoo Cheon <jinwoocheon@yonsei.ac.kr>
2022-09-25 20:58:28 +0900

    Naive way of loading Asset & Image

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import AppLoading from 'expo-app-loading'; // actually depreacted
 import * as SplashScreen from 'expo-splash-screen'; // should change to this later
-import { Text } from 'react-native';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { Asset } from 'expo-asset';
+import { Text, Image } from 'react-native';
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true); // called when AppLoading is finished
   const startLoading = async() => {
     // fetch sth from server/db before loading
-    await new Promise(resolve => setTimeout(resolve, 10000)); // just random 10 secs to make the AppLoading stay
+    // await new Promise(resolve => setTimeout(resolve, 10000)); // just random 10 secs to make the AppLoading stay
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync(require('./image.png'));
   };
 
   if (!ready) {
